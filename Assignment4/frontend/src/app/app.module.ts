a84e06c1085fc720f7ebbb35b769ab62123b089b
Hienology <126324386+Hienology@users.noreply.github.com>
2025-12-08 17:30:38 +0000

    Fix NG0908: Remove HttpClientModule, use only provideHttpClient

--- Assignment4/frontend/src/app/app.module.ts
+++ Assignment4/frontend/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule, ErrorHandler, Injectable } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { OrderReviewComponent } from './components/order-review/order-review.component';
 import { HttpService } from './services/http.service';
 import { ProductService } from './services/product.service';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 // Global error handler to catch uncaught errors
 @Injectable()
@@ -28,13 +27,12 @@ class GlobalErrorHandler implements ErrorHandler {
   imports: [
     BrowserModule,
     CommonModule,
-    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
+    provideHttpClient(withInterceptorsFromDi()),
     HttpService,
     ProductService,
-    provideHttpClient(withInterceptorsFromDi()),
     { provide: ErrorHandler, useClass: GlobalErrorHandler }
   ],
   bootstrap: [AppComponent]
